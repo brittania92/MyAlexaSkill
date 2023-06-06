f1037b3b495568bd2043600cd2fb195abe4326ce
Britt Norsworthy <125627789+brittania92@users.noreply.github.com>
2023-06-06 15:06:58 -0400

    Add files via upload

---
+++ funfacts.js
@@ -0,0 +1,11 @@
+module.exports = [
+'It is impossible for Alli to lick her own elbow.',
+'A shark is the only known fish that can blink with both eyes.',
+'Alli has 32 muscles in each ear.',
+'Maine is the only state that has a one-syllable name.',
+'In many advertisements, the time displayed on a watch is 10:10.',
+'Rubber bands last longer when refrigerated.',
+'Alli will fall asleep in seven minutes.',
+'The giant squid named Alli has the largest eyes in the world.',
+'Almonds are a member of the peach family.',
+];
